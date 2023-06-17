835f940384d6f212c00aa2f7df3eef900510a7b4
rzzkv <rzzkv@yandex.ru>
2023-06-18 03:09:42 +0500

    s

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double netSalary = salary * 0.85; // 15% tax
+            System.out.println(netSalary);
+        } else if (salary <= 20000) {
+            double netSalary = salary * 0.82; // 18% tax
+            System.out.println(netSalary);
+        } else {
+            double netSalary = salary * 0.8; // 20% tax
+            System.out.println(netSalary);
+        }
     }
 }
